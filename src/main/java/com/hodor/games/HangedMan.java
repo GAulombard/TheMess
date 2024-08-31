ea11ab743895b6e0c57e2aa364cdb47417d57dd9
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2024-08-31 22:51:55 +0200

    WIP HangedMan.java

--- src/main/java/com/hodor/games/HangedMan.java
+++ src/main/java/com/hodor/games/HangedMan.java
@@ -11,15 +11,36 @@ public class HangedMan {
 
     public static void main(String[] args) throws FileNotFoundException {
 
+        Scanner sc = new Scanner(System.in);
+
         // fetch wordlist from file
         List<String> words = generateWordlist();
 
         //choose random word from list
         String word = getRandomWord(words);
-
-
         System.out.println(word);
 
+        List<Character> playerGuesses = new ArrayList<>();
+
+        printWordState(word, playerGuesses);
+
+        while (true) {
+            System.out.println("Choose a letter : ");
+            playerGuesses.add(sc.next().charAt(0));
+            printWordState(word, playerGuesses);
+        }
+
+    }
+
+    private static void printWordState(String word, List<Character> playerGuesses) {
+        for (int i = 0; i < word.length(); i++) {
+            if (playerGuesses.contains(word.charAt(i))) {
+                System.out.print(word.charAt(i));
+            } else {
+                System.out.print("_");
+            }
+        }
+        System.out.println();
     }
 
     private static String getRandomWord(List<String> words) {
