00102be0910b9ebd612b9fbaa71cbe2ca57c0c3e
Geoffrey Aulombard <geoffrey.aulombard@gmail.com>
2024-09-29 19:20:15 +0200

    WIP pattern

---
+++ src/main/java/com/hodor/pattern/observer/Car.java
@@ -0,0 +1,19 @@
+package com.hodor.pattern.observer;
+
+import java.util.Observable;
+
+public class Car extends Observable {
+
+    private int speed;
+    public Car(int speed) {
+        this.speed = speed;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public void setSpeed(int speed) {
+        this.speed = speed;
+        setChanged();
+        notifyObservers();
+    }
+}
